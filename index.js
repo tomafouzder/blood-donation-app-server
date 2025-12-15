e06c65543b94354562bb66bc44c118eb790d5ebe
tomafouzder <tomafouzder12@gmail.com>
2025-12-15 20:59:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ioz0rr9.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db('bloodDonationDB')
+        const usersCollection = database.collection('users')
+
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            userInfo.role = 'donor';
+            userInfo.createAt = new Date();
+
+            const result = await usersCollection.insertOne(userInfo)
+            res.send(result)
+        })
+
+        app.get('/users/role/:email', async (req, res) => {
+            const email = req.params.email
+            console.log(email)
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            console.log(result)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Blood Donations')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
