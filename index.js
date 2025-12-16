fcbda0f42c412ab4d547be9d32fed25732c2d5aa
tomafouzder <tomafouzder12@gmail.com>
2025-12-16 21:42:13 +0600

    added this all user api for client get data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
@@ -7,8 +8,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).send({ message: "unauthorize access" })
+    }
+
+    try {
+        const idToken = token.split(' ')[1]
+        const decoded = await admin.auth().verifyIdToken(idToken)
+        console.log("decoded info", decoded)
+        req.decoded_email = decoded.email;
+        next();
+    } catch (error) {
+        return res.status(401).send({ message: "unauthorize access" })
+    }
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ioz0rr9.mongodb.net/?appName=Cluster0`;
 
@@ -30,6 +58,7 @@ async function run() {
         const usersCollection = database.collection('users')
         const requestCollection = database.collection('request')
 
+        // user register data post 
         app.post('/users', async (req, res) => {
             const userInfo = req.body;
             userInfo.role = 'donor';
@@ -39,6 +68,13 @@ async function run() {
             const result = await usersCollection.insertOne(userInfo)
             res.send(result)
         })
+
+        // all user get
+        app.get('/users', verifyFBToken, async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.status(200).send(result)
+        })
+
 
         app.get('/users/role/:email', async (req, res) => {
             const email = req.params.email
@@ -52,7 +88,7 @@ async function run() {
         // Donor apis:
 
         // request
-        app.post('/requests', async (req, res) => {
+        app.post('/requests', verifyFBToken, async (req, res) => {
             const data = req.body;
             data.createAt = new Date();
             const result = await requestCollection.insertOne(data)
